7558412b3228aa28aed18901847e945a7b77ddf8
Annie <93747286+anakolchina97@users.noreply.github.com>
2023-11-24 11:33:59 +0300

    Upd images

--- frontend/src/components/Article/index.tsx
+++ frontend/src/components/Article/index.tsx
@@ -1,12 +1,19 @@
 import { IArticle } from "@/types";
 import styles from "./page.module.scss";
 import React from "react";
+import Image from "next/image";
 
 const Article = ({ src, title, className }: IArticle) => {
   return (
     <article className={`${styles.article} ${className}`}>
       <picture className={styles.article__picture}>
-        <img className={styles.article__img} src={src} />
+        <Image
+          className={styles.article__img}
+          src={src}
+          width={40}
+          height={59}
+          alt=""
+        />
       </picture>
       <p className={styles.article__title}>{title}</p>
     </article>
